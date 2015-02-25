4d17ac67d0551b8d5cadf8b52fb8093e1f169a52
Ribu Royson <ribukaithavana@gmail.com>
2015-02-25 12:46:22 +0530

    layout 24

--- HelloWorld/src/com/ribu/helloworld/MainActivity.java
+++ HelloWorld/src/com/ribu/helloworld/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ribu.helloworld;
 
+import android.R;
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -9,6 +10,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.activity_main);
+		
 	}
 }
